f29cde8e0203f2d8760a28450b9bc32666b80cfc
Nicolas Tran <snicolastran@hotmail.fr>
2023-01-11 10:21:05 +0100

    Docker compose DB and backend
    -Udapte SpringBoot Dockerfile
    -Set Environment variables DATABASE_HOST in properties to use for data base url
    -Creat docker-compose file with DB and backend working

--- Spring-boot_BankProject/src/main/java/com/example/demo/SpringBootAngularApplication.java
+++ Spring-boot_BankProject/src/main/java/com/example/demo/SpringBootAngularApplication.java
@@ -1,15 +1,22 @@
 package com.example.demo;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.expression.EnvironmentAccessor;
+import org.springframework.core.env.Environment;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
+import io.micrometer.observation.Observation.Event;
+
 @SpringBootApplication
 public class SpringBootAngularApplication {
-
+	
 	public static void main(String[] args) {
 		
 		ApplicationContext context =SpringApplication.run(SpringBootAngularApplication.class, args);
